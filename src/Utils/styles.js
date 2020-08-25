fd1954f38bf92073195076570aa2831f2f1573f5
timotheemcdonald <timotheemcdonald@gmail.com>
2020-08-25 14:09:22 -0400

    adding style file

---
+++ src/Utils/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+color:ivory;
+background-color:crimson;
+border:none;
+padding:15px;
+margin:20px auto;
+width:200px;
+`
+
+const StyledInput = styled.input`
+width:190px;`
+
+const CenterDiv = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+background-color:mistyrose;
+width:30%;
+margin:20px auto;
+border:3px crimson solid;
+padding:10px;
+`
+
+const RedSpan = styled.span`
+color: red;
+`
+
+const Details = styled.header`
+
+background-color:crimson;
+color:white;
+min-height:10vh;
+display:flex;
+justify-content:space-around;
+align-items:center;
+
+a {
+  text-decoration:none;
+  color:ivory;
+}
+`
